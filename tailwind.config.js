cba68b680c5a77585a4e000d20056fc1d52141d1
Jessie Bryan Celestial <jcelestial047@gmail.com>
2025-10-29 23:14:59 +0800

    push all files

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    './layout/**/*.{liquid,json}',
+    './sections/**/*.{liquid,json}',
+    './snippets/**/*.{liquid,json}',
+    './templates/**/*.{liquid,json}',
+    './templates/customers/**/*.{liquid,json}',
+    './assets/**/*.js',
+  ],
+  theme: {
+    screens: {
+      sm: '320px',
+      md: '750px',
+      lg: '990px',
+      xlg: '1440px',
+      x2lg: '1920px',
+      pageMaxWidth: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        heading: 'var(--font-heading-family)',
+      },
+    },
+  },
+  plugins: [],
+};
